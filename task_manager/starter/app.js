b4d4cb887764e9c6940ac57262bf655bfdb3ceaa
Matthew Guy Southerland <mgsoutherland529@gmail.com>
2022-12-27 19:02:18 -0500

    feat: added server to app.js

--- task_manager/starter/app.js
+++ task_manager/starter/app.js
@@ -1,1 +1,10 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+
+// set up a public folder
+app.use(express.static("./public"));
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`server is listening on port ${port}...`);
+});
