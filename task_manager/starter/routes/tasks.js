871e2e61e8cf260e8647680238aa7bba5edce61a
Matthew Guy Southerland <mgsoutherland529@gmail.com>
2022-12-27 19:46:56 -0500

    feat: added routes folder and tasks.js file

---
+++ task_manager/starter/routes/tasks.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+
+// const {
+// tasks,
+// task2,
+// taskect
+// } = require('../controllers/tasks')
+// res.status(200).send('Home Page');
+
+// // fill in router.route info
+// router.route('/').get(tasks).post(createTask)
+// router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
+
+module.exports = router;
