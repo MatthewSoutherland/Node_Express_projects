c046fd6d1cb1822b405c5b65af35197f0b465a61
Matthew Guy Southerland <mgsoutherland529@gmail.com>
2022-12-27 19:05:20 -0500

    feat: added home route to app.js

--- task_manager/starter/app.js
+++ task_manager/starter/app.js
@@ -4,6 +4,11 @@ const app = express();
 // set up a public folder
 app.use(express.static("./public"));
 
+app.get("./home", (req, res) => {
+  res.send("Task manager app");
+  res.end();
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`server is listening on port ${port}...`);
