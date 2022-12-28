4057609d264479e6255eb021e0c2478c3afc1ecf
Matthew Guy Southerland <mgsoutherland529@gmail.com>
2022-12-27 19:19:10 -0500

    feat: added todo notes

--- task_manager/starter/app.js
+++ task_manager/starter/app.js
@@ -6,8 +6,13 @@ app.use(express.static("./public"));
 
 app.get("./home", (req, res) => {
   res.send("Task manager app");
-  res.end();
 });
+
+// app.get('/api/v1/tasks')         -get all the tasks
+// app.post('/api/v1/tasks')        -create a new task
+// app.get('/api/v1/tasks/:id')     -get single task
+// app.patch('/api/v1/tasks/:id')   -update task
+// app.delete('/api/v1/tasks/:id')  -delete task
 
 const port = 3000;
 app.listen(port, () => {
