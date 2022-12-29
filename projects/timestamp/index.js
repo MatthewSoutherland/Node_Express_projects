f30c9dcfbece0a443b0bd8cf314babbddd26c34f
Matthew Guy Southerland <mgsoutherland529@gmail.com>
2022-12-29 17:47:40 -0500

    feat: added projects folder

---
+++ projects/timestamp/index.js
@@ -0,0 +1,43 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require("express");
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+// your first API endpoint...
+app.get("/api/timestamp/:timewhore", function (req, res) {
+  let stamp = req.params.timewhore;
+
+  if (stamp.match(/\d{5,}/g)) {
+    stamp = +stamp;
+  }
+
+  let date = new Date(stamp);
+  if (date.toUTCString() == "Invalid Date") {
+    res.json({ unix: date.valueOf(), utc: date.toUTCString() });
+  }
+});
+
+app.get("/api/timestamp/", (req, res) => {
+  let date = new Date();
+  res.json({ unix: date.valueof(), utc: date.toUTCString() });
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
