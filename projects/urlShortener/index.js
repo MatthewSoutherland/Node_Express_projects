9ed87e82e5325249f30f9285e5075a5016f7487c
Matthew Guy Southerland <mgsoutherland529@gmail.com>
2022-12-29 21:19:00 -0500

    feat: finished index.js

--- projects/urlShortener/index.js
+++ projects/urlShortener/index.js
@@ -1,10 +1,26 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const dns = require("dns");
+const urlparser = require("url");
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+
+// connect to database
+mongoose.connect(
+  "mongodb+srv://matt:champ2@nodeexpressprojects.cabrqck.mongodb.net/?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+console.log(mongoose.connection.readyState);
+
+const schema = new mongoose.Schema({ url: "string" });
+const Url = new mongoose.model("Url", schema);
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(cors());
 
@@ -15,8 +31,32 @@ app.get("/", function (req, res) {
 });
 
 // Your first API endpoint
-app.get("/api/hello", function (req, res) {
-  res.json({ greeting: "hello API" });
+app.post("/api/shorturl", function (req, res) {
+  const bodyurl = req.body.url;
+  const dnsLook = dns.lookup(
+    urlparser.parse(bodyurl).hostname,
+    (error, address) => {
+      if (!address) {
+        res.json({ error: "Invalid URL" });
+      } else {
+        const url = new Url({ url: bodyurl });
+        url.save((err, data) => {
+          res.json({ original_url: data.url, short_url: data.id });
+        });
+      }
+    }
+  );
+});
+
+app.get("/api/shorturl/:id", function (req, res) {
+  const id = req.params.id;
+  Url.findById(id, (err, data) => {
+    if (!data) {
+      res.json({ error: "Invalid URL" });
+    } else {
+      res.redirect(data.url);
+    }
+  });
 });
 
 app.listen(port, function () {
